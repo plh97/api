5970f4b32224e184d091a115b4c413e94a6bbc00
pengliheng <pengliheng111@gmail.com>
2018-05-22 01:59:09 +0800

    23r

--- routes/graphql.js
+++ routes/graphql.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 
 const Graphql = async (ctx) => {
   const query = ctx.request.body;
+  console.log(process.env.access_token)
   const queryFunc = async data => new Promise((resolve, reject) => {
     axios({
       url: 'https://api.github.com/graphql',
