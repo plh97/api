512664c2fc2966be2954113cb6288c9cfed071a1
pengliheng <pengliheng111@gmail.com>
2018-05-23 23:20:21 +0800

    12e

--- routes/graphql.js
+++ routes/graphql.js
@@ -1,27 +1,37 @@
 const axios = require('axios');
 
+const cache = {}
+
 const Graphql = async (ctx) => {
   const query = ctx.request.body;
   const queryFunc = async data => new Promise((resolve, reject) => {
-    axios({
-      url: 'https://api.github.com/graphql',
-      method: 'post',
-      headers: {
-        Authorization: `bearer ${process.env.access_token}`,
-        'Content-Type': 'application/json',
-      },
-      data,
-    })
-      .then((res) => {
-        resolve({
-          code: 200,
-          data: res.data,
-        });
-      }, (err) => {
-        console.log(err);
-        console.log(process.env.access_token);
-        reject(err);
+    if(cache[data]){
+      resolve({
+        code: 200,
+        data: cache[data],
       });
+    } else {
+      axios({
+        url: 'https://api.github.com/graphql',
+        method: 'post',
+        headers: {
+          Authorization: `bearer ${process.env.access_token}`,
+          'Content-Type': 'application/json',
+        },
+        data,
+      })
+        .then( res => {
+          resolve({
+            code: 200,
+            data: res.data,
+          });
+        }, err => {
+          reject({
+            code: 500,
+            err
+          });
+        });
+    }
   });
   ctx.body = await queryFunc(query);
 };
