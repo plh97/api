98ce210890008812dad01fde379df3b47172c45b
pengliheng <pengliheng111@gmail.com>
2018-05-24 21:18:04 +0800

    23r

--- routes/graphql.js
+++ routes/graphql.js
@@ -1,12 +1,39 @@
 const axios = require('axios');
-global.cache = {}
+
+class Cache {
+  constructor(){
+    this.cache = [];
+  }
+
+  getCache(key){
+    this.cache.find(e => e.key === key);
+  }
+
+  addCache({key,val}){
+    // 添加
+    const isExist = this.getCache(key);
+    if(isExist.length>0){
+      // 如果有数组，那就直接返回它，
+      return isExist
+    } else {
+      // 如果没有，那么push
+      this.cache.push({key,val});
+      return {key,val}
+    }
+  }
+}
+
+const cache = new Cache()
+
+
 const Graphql = async (ctx) => {
   const query = ctx.request.body;
   const queryFunc = async data => new Promise((resolve, reject) => {
     console.log('是否相等')
-    console.log(cache[data])
+    console.log(cache.getCache(data))
     console.log(data)
-    if(cache[data]){
+    const isExist = cache.getCache(data);
+    if(isExist){
       console.log('get data from cache')
       resolve(cache[data]);
     } else {
@@ -21,7 +48,10 @@ const Graphql = async (ctx) => {
         data,
       })
         .then( res => {
-          cache[data] = res.data,
+          cache.push({
+            key: data,
+            val: res.data
+          });
           resolve(res.data);
         });
     }
