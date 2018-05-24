1448bd5ac6bdaad18dca1213586b1285feb2f7d2
pengliheng <pengliheng111@gmail.com>
2018-05-24 20:32:02 +0800

    change api url

--- routes/graphql.js
+++ routes/graphql.js
@@ -5,7 +5,7 @@ const Graphql = async (ctx) => {
   const queryFunc = async data => new Promise((resolve, reject) => {
     console.log(
       '是否相等',
-      Object.keys(cache)  === data
+      Object.keys(cache).includes(data)
     )
     if(cache[data]){
       console.log('get data from cache')
